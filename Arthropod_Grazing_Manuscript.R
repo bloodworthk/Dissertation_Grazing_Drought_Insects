18b637b417dfe294be96fa543e32df098dbd093f
Bloodworthk <bloodworthk@si.edu>
2023-09-20 15:24:51 -0400

    Make feeding guild graphs

--- Arthropod_Grazing_Manuscript.R
+++ Arthropod_Grazing_Manuscript.R
@@ -76,11 +76,15 @@ Weight_Data_22<-read.csv("2022_Sweep_Net_D-Vac_Weight_Data_FK.csv",header=T) %>%
   mutate(Collection_Method=ifelse(Collection_Method=="d-vac","dvac",ifelse(Collection_Method=="sweep_net","sweep",Collection_Method)))%>% 
   filter(Collection_Method=="dvac")
 
+#Arthropod Feeding Guild Data
+Feeding<-read.csv("Arthropod_ID_Data_Guilds.csv", header=T)
+
 #Plant Species Comp Data 
 PlantComp<-read.csv("Plant_Species_Comp_2022.csv",header=T) 
 
 
 Functional_Groups<-read.csv("FunctionalGroups.csv")
+
 
 #### Formatting and Cleaning ID Data ####
 
@@ -158,17 +162,31 @@ ID_Data_Official<-ID_20 %>%
 Abundance<-ID_Data_Official %>% 
   group_by(Collection_Method,Year,Block,Grazing_Treatment,Plot,Correct_Order) %>% 
   mutate(Abundance=length(Sample_Number)) %>% 
-  ungroup() 
+  ungroup()
 
 Abundance_Plot<-ID_Data_Official %>% 
   group_by(Collection_Method,Year,Block,Grazing_Treatment,Plot) %>% 
   mutate(Plot_Abundance=length(Sample_Number)) %>% 
   ungroup() %>% 
   select(Collection_Method,Year,Block,Grazing_Treatment,Plot,Plot_Abundance) %>% 
-  unique() 
+  unique()
 
 Abundance_Order<-Abundance %>% 
   select(Collection_Method,Year,Block,Grazing_Treatment,Correct_Order,Plot,Abundance) %>% 
+  unique() 
+
+#### Abundance by Count: Family ####
+Abundance_Family<-ID_Data_Official %>% 
+  group_by(Collection_Method,Year,Block,Grazing_Treatment,Plot,Correct_Order,Correct_Family) %>% 
+  mutate(Abundance=length(Sample_Number)) %>% 
+  ungroup() %>% 
+  left_join(Feeding) %>% 
+  select(Collection_Method,Year,Block,Plot, Grazing_Treatment,Correct_Order,Correct_Family, Feeding.Guild,Abundance) %>% 
+  unique() 
+
+
+Abundance_Family<-Abundance %>% 
+  select(Collection_Method,Year,Block,Grazing_Treatment,Correct_Order,Correct_Family, Plot,Abundance) %>% 
   unique() 
 
 
@@ -941,8 +959,6 @@ BC_Distance_Matrix_Weight <- vegdist(Species_Matrix_Weight)
 Dispersion_Results_Grazing_Weight <- betadisper(BC_Distance_Matrix_Weight,Abundance_Wide_Weight_dispr$Gr_Yr)
 permutest(Dispersion_Results_Grazing_Weight,pairwise = T, permutations = 999) 
 
-
-
 #### Supp Doc Figures ####
 #### Supp Doc Figure 1. (A,B,C) Richness, (D,E,F) Evenness of Arthropod Community ####
 
@@ -1434,3 +1450,90 @@ RelCov_GLMM <- lmerTest::lmer(data = FG_RelCov, log(Relative_Cover) ~ grazing_tr
 anova(RelCov_GLMM, type = 3) #0.03647
 # post hoc test for lmer test
 summary(glht(RelCov_GLMM, linfct = mcp(grazing_treatment = "Tukey")), test = adjusted(type = "BH")) #NG-LG (p=0.5056), #LG-HG (0.1093), NG-HG (0.0351)
+
+
+#### Feeding Guild Graph ####
+
+Relative_Count_Family<-Abundance_Family %>% 
+  filter(Plot!="NA" & Correct_Family!="NA") %>% 
+  filter(Correct_Order!="unknown"&Correct_Order!="Unknown"&Correct_Order!="Unknown_1"&Correct_Order!="Body_Parts"&Correct_Order!="Body Parts" & Correct_Family!="Unknown") %>% 
+  select(Year,Block,Grazing_Treatment,Plot,Correct_Order,Correct_Family,Feeding.Guild,Abundance) %>% 
+  unique() %>% 
+  #add together all data of each orders across grazing treatments 
+  group_by(Year,Grazing_Treatment,Feeding.Guild) %>% 
+  mutate(FeedingGuild_Abundance=sum(Abundance)) %>%
+  ungroup() %>% 
+  #add together all data within each grazing treatment for total "plot"count
+  group_by(Year,Grazing_Treatment) %>% 
+  mutate(Total_Abundance=sum(Abundance)) %>%
+  ungroup() %>% 
+  select(Year,Grazing_Treatment,Feeding.Guild,FeedingGuild_Abundance,Total_Abundance) %>% 
+  unique() %>% 
+  mutate(RelativeCount=FeedingGuild_Abundance/Total_Abundance) %>% 
+  group_by(Year,Grazing_Treatment,Feeding.Guild) %>% 
+  summarise(Average_RelativeCount=mean(RelativeCount)) %>% 
+  ungroup() %>% 
+  mutate(Grazing_Treatment=ifelse(Grazing_Treatment=="HG","High Impact Grazing",ifelse(Grazing_Treatment=="LG","Destock Grazing",ifelse(Grazing_Treatment=="NG","Cattle Removal",Grazing_Treatment)))) %>% 
+  mutate(Feeding_Guild_Graph=ifelse(Feeding.Guild=="Insectivores","Predators",ifelse(Feeding.Guild=="","Other",ifelse(Feeding.Guild=="Multi","Other",ifelse(Feeding.Guild=="necrophagous","Scavenger",ifelse(Feeding.Guild=="parasitoids","Parasitoids",ifelse(Feeding.Guild=="phytophagous","Herbivores",Feeding.Guild)))))))
+
+
+##reorder bar graphs##
+Relative_Count_Family$Grazing_Treatment <- factor(Relative_Count_Family$Grazing_Treatment, levels = c("Cattle Removal", "Destock Grazing", "High Impact Grazing"))
+
+Feeding_Guild_2020<-ggplot(subset(Relative_Count_Family,Year==2020),aes(x=Grazing_Treatment,y=Average_RelativeCount,fill=Feeding_Guild_Graph, position = "stack"))+
+  geom_bar(stat="identity")+
+  #Label the x-axis "Treatment"
+  xlab("Grazing Regime")+
+  #Label the y-axis "Species Richness"
+  ylab("Proportion of Feeding Guilds")+
+  theme(legend.background=element_blank())+
+  scale_x_discrete(labels = function(x) str_wrap(x, width = 8))+
+  scale_fill_manual(values=c("#7B5E7B","#A6A670","#1C1F33", "#C0B9DD","#86836D","#626267","#710000"), labels=c("Fungivores","Herbivores","Omnivores","Parisitoids","Predators","Scavenger","Other"), name = "Feeding Guild")+
+  #scale_fill_manual(values=c("grey30","grey10"), labels=c("Orthoptera Count","Plot Count"))+
+  theme(legend.key = element_rect(size=3), legend.key.size = unit(1,"centimeters"),legend.position="NONE")+
+  #Make the y-axis extend to 50
+  expand_limits(y=1.2)+
+  scale_y_continuous(labels = label_number(accuracy = 0.25))+
+  theme(text = element_text(size = 55),legend.text=element_text(size=45))+
+  geom_text(x=1, y=1.2,label="A.2020",size=20)
+
+Feeding_Guild_2021<-ggplot(subset(Relative_Count_Family,Year==2021),aes(x=Grazing_Treatment,y=Average_RelativeCount,fill=Feeding_Guild_Graph, position = "stack"))+
+  geom_bar(stat="identity")+
+  #Label the x-axis "Treatment"
+  xlab("Grazing Regime")+
+  #Label the y-axis "Species Richness"
+  ylab("Proportion of Feeding Guilds")+
+  theme(legend.background=element_blank())+
+  scale_x_discrete(labels = function(x) str_wrap(x, width = 8))+
+  scale_fill_manual(values=c("#7B5E7B","#A6A670","#1C1F33", "#C0B9DD","#86836D","#626267","#710000"), labels=c("Fungivores","Herbivores","Omnivores","Parisitoids","Predators","Scavenger","Other"), name = "Feeding Guild")+
+  #scale_fill_manual(values=c("grey30","grey10"), labels=c("Orthoptera Count","Plot Count"))+
+  theme(legend.key = element_rect(size=3), legend.key.size = unit(1,"centimeters"),legend.position="NONE")+
+  #Make the y-axis extend to 50
+  expand_limits(y=1.2)+
+  scale_y_continuous(labels = label_number(accuracy = 0.25))+
+  theme(text = element_text(size = 55),legend.text=element_text(size=45),axis.title.y=element_blank(),axis.text.y=element_blank())+
+  geom_text(x=1, y=1.2,label="B.2021",size=20)
+
+Feeding_Guild_2022<-ggplot(subset(Relative_Count_Family,Year==2022),aes(x=Grazing_Treatment,y=Average_RelativeCount,fill=Feeding_Guild_Graph, position = "stack"))+
+  geom_bar(stat="identity")+
+  #Label the x-axis "Treatment"
+  xlab("Grazing Regime")+
+  #Label the y-axis "Species Richness"
+  ylab("Proportion of Feeding Guilds")+
+  theme(legend.background=element_blank())+
+  scale_x_discrete(labels = function(x) str_wrap(x, width = 8))+
+  scale_fill_manual(values=c("#7B5E7B","#A6A670","#1C1F33", "#C0B9DD","#86836D","#626267","#710000"), labels=c("Fungivores","Herbivores","Omnivores","Parisitoids","Predators","Scavenger","Other"), name = "Feeding Guild")+
+  #scale_fill_manual(values=c("grey30","grey10"), labels=c("Orthoptera Count","Plot Count"))+
+  theme(legend.key = element_rect(size=3), legend.key.size = unit(1,"centimeters"),legend.position="NONE")+
+  #Make the y-axis extend to 50
+  expand_limits(y=1.2)+
+  scale_y_continuous(labels = label_number(accuracy = 0.25))+
+  theme(text = element_text(size = 55),legend.text=element_text(size=45),axis.title.y=element_blank(),axis.text.y=element_blank())+
+  geom_text(x=1, y=1.2,label="C.2022",size=20)
+
+#### Create Feeding Guild Graph ####
+Feeding_Guild_2020+
+  Feeding_Guild_2021+
+  Feeding_Guild_2022
+  plot_layout(ncol = 3,nrow = 1)
+#Save at 3000x2000
